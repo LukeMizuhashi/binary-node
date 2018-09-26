21d647dc853d27b9177011ef5481776ccd09475c
Luke Mizuhashi <luke.mizuhashi@nable.org>
2018-09-26 08:03:22 +0000

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,20 @@
+global.BinaryNode = require('../src/constructor.js');
+global.assert = require('chai').assert;
+
+const describeConstructor = require('./constructor/description.js');
+const describeIsLeaf = require('./isLeaf/description.js');
+const describeDestroy = require('./destroy/description.js');
+const describeReplaceWith = require('./replaceWith/description.js');
+const describeSet = require('./set/description.js');
+const describeCompare = require('./compare/description.js');
+
+describe('BinarySearchTree',() => {
+
+  describeConstructor();
+  describe('BinaryNode.isLeaf',describeIsLeaf);
+  describe('BinaryNode.destroy',describeDestroy);
+  describe('BinaryNode.replaceWith',describeReplaceWith);
+  describe('BinaryNode.set',describeSet);
+  describe('BinaryNode.compare',describeCompare);
+});
+
